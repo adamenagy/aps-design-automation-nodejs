d8122a4f4dc6f0d4e0740078694ec7f1f83373bd
Adam Nagy <adam_e_nagy@hotmail.com>
2023-11-07 12:11:53 +0000

    init

--- config.js
+++ config.js
@@ -1,28 +1,26 @@
-// Autodesk Forge configuration
+let { APS_CLIENT_ID, APS_CLIENT_SECRET, PORT } = process.env;
+if (!APS_CLIENT_ID || !APS_CLIENT_SECRET) {
+    console.warn('Missing some of the environment variables.');
+    process.exit(1);
+}
+PORT = PORT || 8080;
+
+DA_CLIENT_CONFIG = {
+  circuitBreaker: {
+      threshold: 11,
+      interval: 1200
+  },
+  retry: {
+      maxNumberOfRetries: 7,
+      backoffDelay: 4000,
+      backoffPolicy: 'exponentialBackoffWithJitter'
+  },
+  requestTimeout: 13000
+};
+
 module.exports = {
-    // Set environment variables or hard-code here
-    credentials: {
-        client_id: process.env.APS_CLIENT_ID,
-        client_secret: process.env.APS_CLIENT_SECRET,
-        callback_url: process.env.APS_CALLBACK || process.env.APS_CALLBACK_URL,
-        webhook_url: process.env.APS_WEBHOOK_URL
-    },
-    scopes: {
-        // Required scopes for the server-side application
-        internal: ['bucket:create', 'bucket:read', 'bucket:delete', 'data:read', 'data:create', 'data:write', 'code:all'],
-        // Required scope for the client-side viewer
-        public: ['viewables:read']
-    },
-    client: {
-        circuitBreaker: {
-            threshold: 11,
-            interval: 1200
-        },
-        retry: {
-            maxNumberOfRetries: 7,
-            backoffDelay: 4000,
-            backoffPolicy: 'exponentialBackoffWithJitter'
-        },
-        requestTimeout: 13000
-    }
+    APS_CLIENT_ID,
+    APS_CLIENT_SECRET,
+    PORT,
+    DA_CLIENT_CONFIG
 };
---
+++ routes/setup.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { getEngines, getLocalAppBundles, getActivities, setup, deleteAccount } = require('../services/aps.js');
+
+let router = express.Router();
+
+router.use(bodyParser.json());
+
+router.get('/api/engines', async function (req, res, next) {
+  try {
+    const engines = await getEngines();
+    res.json(engines);
+  } catch (err) {
+    res.statusMessage = err;
+    res.status(500).end();
+  }
+});
+
+router.get('/api/appbundles', async function (req, res, next) {
+  try {
+     const appbundles = await getLocalAppBundles();
+      res.json(appbundles);
+  } catch (err) {
+    res.statusMessage = err;
+    res.status(500).end();
+  }
+});
+
+router.get('/api/activities', async function (req, res, next) {
+  try {
+     const activities = await getActivities();
+      res.json(activities);
+  } catch (err) {
+    res.statusMessage = err;
+    res.status(500).end();
+  }
+});
+
+router.post('/api/setup', async function (req, res, next) {
+  try {
+     const response = await setup(req.body);
+      res.json(response);
+  } catch (err) {
+    res.statusMessage = err;
+    res.status(500).end();
+  }
+});
+
+router.delete('/api/account', async function (req, res, next) {
+  try {
+     await deleteAccount();
+      res.end();
+  } catch (err) {
+    res.statusMessage = err;
+    res.status(500).end();
+  }
+});
+
+module.exports = router;
