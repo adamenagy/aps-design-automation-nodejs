aa98195cc11b875d00654444c462c60c65140809
Adam Nagy <adam_e_nagy@hotmail.com>
2023-11-07 12:32:17 +0000

    format

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-const express = require('express');
-const { PORT } = require('./config.js');
+const express = require("express");
+const { PORT } = require("./config.js");
 
 let app = express();
-app.use(express.static('wwwroot'));
-app.use(require('./routes/workitems.js'));
-app.use(require('./routes/setup.js'));
-app.listen(PORT, function () { console.log(`Server listening on port ${PORT}...`); });
+app.use(express.static("wwwroot"));
+app.use(require("./routes/workitems.js"));
+app.use(require("./routes/setup.js"));
+app.listen(PORT, function () {
+	console.log(`Server listening on port ${PORT}...`);
+});
